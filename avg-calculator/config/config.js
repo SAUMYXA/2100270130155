64a7eb23d6e1ddb823b132d3afb8c53d546ed520
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2024-06-08 10:42:39 +0530

    adding config

---
+++ avg-calculator/config/config.js
@@ -0,0 +1,14 @@
+const config = {
+    PORT: 9876,
+    WINDOW_SIZE: 10,
+    TIMEOUT: 500,
+    testServerUrls: {
+      p: 'http://20.244.56.144/test/primes',
+      P: 'http://20.244.56.144/test/fibo',
+      e: 'http://20.244.56.144/test/even',
+      r: 'http://20.244.56.144/test/rand'
+    }
+  };
+  
+  module.exports = config;
+  
